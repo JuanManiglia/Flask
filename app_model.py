eea5b3bab68ffa0dbcbc3146f5ae5e29627db1ba
JuanManiglia <jamr1989@gmail.com>
2022-09-09 14:04:39 +0200

    git_udate

--- app_model.py
+++ app_model.py
@@ -1,13 +1,24 @@
+from crypt import methods
 from flask import Flask, jsonify, request
 import os
 import pandas as pd
 import numpy as np
 import pickle
+import git
 
 os.chdir(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./Flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/',methods=['GET'])
 def hello():
