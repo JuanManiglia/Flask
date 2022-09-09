e54137f7f5975c3adfeb9fe273025b68fd9664fc
JuanManiglia <jamr1989@gmail.com>
2022-09-09 14:06:50 +0200

    cambio para probar

--- app_model.py
+++ app_model.py
@@ -22,7 +22,7 @@ def git_update():
 
 @app.route('/',methods=['GET'])
 def hello():
-    return "Mi primera API Flask CUTRE"
+    return "Mi primera API Flask CUTRE con github"
 
 @app.route('/api/v1/predict', methods=['GET'])
 def predict():
