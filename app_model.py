be8a9159bd9113826fd9dfb3e7a7152f78541e00
JuanManiglia <jamr1989@gmail.com>
2022-09-10 12:58:22 +0200

    sin boton

--- app_model.py
+++ app_model.py
@@ -17,7 +17,7 @@ def git_update():
 
 @app.route('/',methods=['GET'])
 def hello():
-    return "Mi primera API Flask CUTRE pero actualiza desde visual"
+    return "Mi primera API Flask CUTRE pero actualiza desde visual y sin apretar el boton"
 
 
 
