6fc049edbeff9694ed1e7c3d61e02c971697b393
JuanManiglia <jamr1989@gmail.com>
2022-09-10 10:53:56 +0200

    atualizacion

--- app_model.py
+++ app_model.py
@@ -8,6 +8,17 @@ os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./Flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route('/',methods=['GET'])
 def hello():
     return "Mi primera API Flask CUTRE act"
