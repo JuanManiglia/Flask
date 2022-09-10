fdd36866527e66a34516c7991088038753db4997
JuanManiglia <jamr1989@gmail.com>
2022-09-10 12:08:17 +0200

    hola

--- app_model.py
+++ app_model.py
@@ -17,7 +17,7 @@ def git_update():
 
 @app.route('/',methods=['GET'])
 def hello():
-    return "Mi primera API Flask CUTRE act desde github.."
+    return "Mi primera API Flask CUTRE act desde github hola"
 
 
 
