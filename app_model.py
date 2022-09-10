96406296381c308cfa1c2fa7710922bef5da169b
JuanManiglia <jamr1989@gmail.com>
2022-09-10 12:01:18 +0200

    primero

--- app_model.py
+++ app_model.py
@@ -1,23 +1,5 @@
 from flask import Flask
 import os
-import git
-
-
-os.chdir(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-
-# Route for the GitHub webhook
-
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    repo = git.Repo('./Flask')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
 
 @app.route('/',methods=['GET'])
 def hello():
