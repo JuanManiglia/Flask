7720f2d080637e05a226bb975ac35b24105483ed
JuanManiglia <jamr1989@gmail.com>
2022-09-10 12:49:56 +0200

    git_update

--- app_model.py
+++ app_model.py
@@ -1,8 +1,20 @@
 from flask import Flask
 import os
+import git
 
 app = Flask(__name__)
 
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./Flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+    
 @app.route('/',methods=['GET'])
 def hello():
     return "Mi primera API Flask CUTRE act desde github hola"
